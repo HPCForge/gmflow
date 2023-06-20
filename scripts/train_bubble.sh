3a36acf6ef59cf237e1c0b311ab7d2a29fd1963c
Sheikh Md Shakeel Hassan <115192619+shassan97@users.noreply.github.com>
2023-06-19 19:21:37 -0700

    Update train_bubble.sh

--- scripts/train_bubble.sh
+++ scripts/train_bubble.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #C+B
 python main.py --checkpoint_dir chairs_boil/ --resume pretrained/gmflow_chairs-1d776046.pth --stage boiling --batch_size 4 --num_workers 4  --lr 1e-6 --weight_decay 1e-6 --image_size 512 512  --val_freq 100 --val_dataset chairs things sintel kitti boiling  --save_ckpt_freq 100 --num_steps 2000
 #C+T+B
